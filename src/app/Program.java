02fa751df2e096b525dadffdd2590661e1e9f8a4
NicolasHLinck <nciolashlinck@gmail.com>
2025-07-21 20:49:59 -0300

    Update Implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -38,6 +38,15 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("INSERTED! New ID = "+newSeller.getId());
+		
+		System.out.println("\n TESTE 5 -> SELLER UPDATE");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("UPDATE COMPLETED WITH SUCCESS");
+			
+		
+		
 	
 	}
 
